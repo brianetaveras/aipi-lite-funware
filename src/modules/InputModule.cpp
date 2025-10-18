84413d973c5379b3172b46e3eabbbcd19fdf19b2
brianetaveras <taveras574@gmail.com>
2025-10-18 13:47:11 -0400

    Added debug NVS erasure on long button press, renamed images to sprite

--- src/modules/InputModule.cpp
+++ src/modules/InputModule.cpp
@@ -1,13 +1,18 @@
 #include "config/pins.h"
 #include <Arduino.h>
 #include "modules/Display.h"
+#include "modules/DevicePreferences.h"
 
 volatile bool mainButtonPressed = false;
+static unsigned long buttonPressDuration = 0;
+#define LONG_PRESS_DURATION 10000 // 10 seconds in milliseconds
+
 
 void IRAM_ATTR handleButtonPress()
 {
 
-    mainButtonPressed = !mainButtonPressed;
+    mainButtonPressed = true;
+    buttonPressDuration = millis();
     wakeScreen();
 }
 
@@ -17,8 +22,33 @@ void setupInput()
     pinMode(ACTION_BTN_1_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(ACTION_BTN_1_PIN), handleButtonPress, FALLING);
 }
+void onButtonLongPress()
+{
+    eraseNVS();
+}
+
+
+void checkButtonLongPress()
+{
+    if (mainButtonPressed == false)
+    {
+        return;
+    }
+
+    unsigned long currentTime = millis();
+    unsigned long heldTime = currentTime - buttonPressDuration;
+    if (heldTime >= LONG_PRESS_DURATION)
+    {
+        onButtonLongPress();
+        mainButtonPressed = false; // Reset the flag after handling long press
+    }
+}
 
 void inputLoop()
 {
-    // Placeholder for input handling logic
+    checkButtonLongPress();
+    if (mainButtonPressed && digitalRead(ACTION_BTN_1_PIN) == HIGH) {
+        mainButtonPressed = false;
+        buttonPressDuration = 0;
+    }
 }
